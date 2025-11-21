c9593d881836ebb923c8473764bab38412927397
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2025-11-21 22:47:28 +0900

    chore: change home page

--- src/routes/(website)/index.tsx
+++ src/routes/(website)/index.tsx
@@ -1,15 +1,12 @@
-import { Button } from "@miyauci/material-react";
 import { type JSX } from "react";
-import Error from "@/components/Error.tsx";
 
 export default function Home(): JSX.Element {
   return (
     <div id="root">
       <div>
-        <Button>Label</Button>
+        {/* <Button>Label</Button> */}
         <a href="/admin">Admin</a>
         <a href="/about">About</a>
-        <Error />
       </div>
     </div>
   );
