cf5f707753d2aaec9639d315e6dea8e88825d94d
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2025-11-23 14:52:19 +0900

    feat: add app as service

---
+++ src/services/app.tsx
@@ -0,0 +1,16 @@
+import { type FallbackProps } from "error-boundary";
+import { type JSX, lazy } from "react";
+import { isNotFoundErrorLike } from "react-app";
+
+const NotFound = /* @__PURE__ */ lazy(() => import("@/routes/_404.tsx"));
+const ServerError = /* @__PURE__ */ lazy(() => import("@/routes/_500.tsx"));
+
+export function Fallback(props: FallbackProps): JSX.Element {
+  const { error, reset } = props;
+
+  if (isNotFoundErrorLike(error)) {
+    return <NotFound error={error} reset={reset} />;
+  }
+
+  return <ServerError error={error} reset={reset} />;
+}
