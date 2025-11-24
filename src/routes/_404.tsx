3f807553bc3c48532628d333d4d52774bf0061e3
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2025-11-24 09:45:04 +0900

    chore: remove layout deps from not found page

--- src/routes/_404.tsx
+++ src/routes/_404.tsx
@@ -1,12 +1,13 @@
 import type { JSX } from "react";
-import Layout from "@/routes/(website)/_layout.tsx";
 
 export default function NotFound(): JSX.Element {
   return (
-    <Layout>
-      <main>
-        <h1>Not Found</h1>
-      </main>
-    </Layout>
+    <html>
+      <body>
+        <main>
+          <h1>Not Found</h1>
+        </main>
+      </body>
+    </html>
   );
 }
