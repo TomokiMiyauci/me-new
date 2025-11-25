d5fd5ff394bb64d992dbb7d8e20b29f93b63aea6
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2025-11-25 23:48:07 +0900

    refactor: wrap get env and assert process

--- src/env.ts
+++ src/env.ts
@@ -1,13 +1,26 @@
 import { assert } from "@std/assert/assert";
 
-export const SANITY_ID = /* @__PURE__ */ Deno.env.get("SANITY_ID")!;
-export const SANITY_DATASET = /* @__PURE__ */ Deno.env.get("SANITY_DATASET")!;
+function loadSanityId(): string {
+  const SANITY_ID = /* @__PURE__ */ Deno.env.get("SANITY_ID");
+
+  assert(SANITY_ID, "SANITY_ID is not defined");
+
+  return SANITY_ID;
+}
+
+function loadSanityDataset(): string {
+  const SANITY_DATASET = /* @__PURE__ */ Deno.env.get("SANITY_DATASET");
+
+  assert(SANITY_DATASET, "SANITY_DATASET is not defined");
+
+  return SANITY_DATASET;
+}
+
+export const SANITY_ID = /* @__PURE__ */ loadSanityId();
+export const SANITY_DATASET = /* @__PURE__ */ loadSanityDataset();
 export const SENTRY_DSN = /* @__PURE__ */ Deno.env.get("SENTRY_DSN");
 export const SENTRY_ENV = /* @__PURE__ */ Deno.env.get("SENTRY_ENV");
 export const CSP_ENDPOINT = /* @__PURE__ */ Deno.env.get("CSP_ENDPOINT");
-
-/* @__PURE__ */ assert(SANITY_ID, "SANITY_ID is not defined");
-/* @__PURE__ */ assert(SANITY_DATASET, "SANITY_DATASET is not defined");
 
 export const PUBLIC = {
   SANITY_ID,
