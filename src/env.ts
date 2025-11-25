0147427fa78126a7bdd311b93d14c70f9d3fa0ba
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2025-11-25 14:45:22 +0900

    chore: add assert for env

--- src/env.ts
+++ src/env.ts
@@ -1,8 +1,13 @@
-export const SANITY_ID = /* @__PURE__ */ Deno.env.get("SANITY_ID");
-export const SANITY_DATASET = /* @__PURE__ */ Deno.env.get("SANITY_DATASET");
+import { assert } from "@std/assert/assert";
+
+export const SANITY_ID = /* @__PURE__ */ Deno.env.get("SANITY_ID")!;
+export const SANITY_DATASET = /* @__PURE__ */ Deno.env.get("SANITY_DATASET")!;
 export const SENTRY_DSN = /* @__PURE__ */ Deno.env.get("SENTRY_DSN");
 export const SENTRY_ENV = /* @__PURE__ */ Deno.env.get("SENTRY_ENV");
 export const CSP_ENDPOINT = /* @__PURE__ */ Deno.env.get("CSP_ENDPOINT");
+
+/* @__PURE__ */ assert(SANITY_ID, "SANITY_ID is not defined");
+/* @__PURE__ */ assert(SANITY_DATASET, "SANITY_DATASET is not defined");
 
 export const PUBLIC = {
   SANITY_ID,
