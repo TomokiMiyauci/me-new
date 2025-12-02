7dde373c604644de92c1bb0ddb150fe673e09f06
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2025-12-02 09:33:26 +0900

    refactor: move sanity config

--- src/routes/(sanity)/admin.tsx
+++ src/routes/(sanity)/admin.tsx
@@ -1,5 +1,5 @@
 import { Studio } from "sanity_rsc";
-import config from "~/sanity.config.ts";
+import config from "@/sanity.config.ts";
 import { type JSX } from "react";
 
 export default function Admin(): JSX.Element {
