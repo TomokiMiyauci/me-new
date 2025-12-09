098eed56f11a6a1a8c465d44f1f33fb18a38aca3
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2025-12-09 17:37:15 +0900

    chore: add env value

--- src/env.ts
+++ src/env.ts
@@ -16,15 +16,25 @@ function loadSanityDataset(): string {
   return SANITY_DATASET;
 }
 
+function loadContentEndpoint(): string {
+  const CONTENT_ENDPOINT = /* @__PURE__ */ Deno.env.get("CONTENT_ENDPOINT");
+
+  assert(CONTENT_ENDPOINT, "CONTENT_ENDPOINT is not defined");
+
+  return CONTENT_ENDPOINT;
+}
+
 export const SANITY_ID = /* @__PURE__ */ loadSanityId();
 export const SANITY_DATASET = /* @__PURE__ */ loadSanityDataset();
 export const SENTRY_DSN = /* @__PURE__ */ Deno.env.get("SENTRY_DSN");
 export const SENTRY_ENV = /* @__PURE__ */ Deno.env.get("SENTRY_ENV");
 export const CSP_ENDPOINT = /* @__PURE__ */ Deno.env.get("CSP_ENDPOINT");
+export const CONTENT_ENDPOINT = /* @__PURE__ */ loadContentEndpoint();
 
 export const PUBLIC = {
   SANITY_ID,
   SANITY_DATASET,
   SENTRY_ENV,
   SENTRY_DSN,
+  CONTENT_ENDPOINT,
 };
