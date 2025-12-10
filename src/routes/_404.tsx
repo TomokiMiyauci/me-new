ee0cbb9217e3a4cb77c1a68765c8e1e05ebd6c97
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2025-12-11 01:26:26 +0900

    chore: change not found and error component

--- src/routes/_404.tsx
+++ src/routes/_404.tsx
@@ -2,12 +2,8 @@ import type { JSX } from "react";
 
 export default function NotFound(): JSX.Element {
   return (
-    <html>
-      <body>
-        <main>
-          <h1>Not Found</h1>
-        </main>
-      </body>
-    </html>
+    <main>
+      <h1>Not Found</h1>
+    </main>
   );
 }
--- src/routes/_500.tsx
+++ src/routes/_500.tsx
@@ -1,21 +1,17 @@
-"use client";
-import { type FallbackProps } from "react-error-boundary";
 import { JSX } from "react";
 
-export default function ServerError(props: FallbackProps): JSX.Element {
+export default function ServerError(): JSX.Element {
   return (
-    <html>
-      <body>
-        <main>
-          <h1>Server Error</h1>
-          {import.meta.env.DEV && <p>{props.error.message}</p>}
-          {import.meta.env.DEV && <p>{props.error.stack}</p>}
+    <main>
+      <h1>Server Error</h1>
+      {
+        /* {import.meta.env.DEV && <p>{props.error.message}</p>}
+      {import.meta.env.DEV && <p>{props.error.stack}</p>}
 
-          <button type="button" onClick={() => props.resetErrorBoundary()}>
-            rest
-          </button>
-        </main>
-      </body>
-    </html>
+      <button type="button" onClick={() => props.resetErrorBoundary()}>
+        rest
+      </button> */
+      }
+    </main>
   );
 }
