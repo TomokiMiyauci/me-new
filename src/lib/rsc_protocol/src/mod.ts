d1eeec12496cd4cc0db62d3706ff17d9ddd0b1a6
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2025-12-13 14:56:08 +0900

    refactor(rsc-protocol): change import specifier

--- src/lib/rsc_protocol/src/mod.ts
+++ src/lib/rsc_protocol/src/mod.ts
@@ -2,8 +2,8 @@ export { RscRequest, type RscRequestInit } from "./request.ts";
 export { parseRequest } from "./negotiator.ts";
 export { RscResponse } from "./response.ts";
 export type { Action, ReturnValue, RscPayload } from "./types.ts";
-export { default as Rsc, type RscProps } from "./components/Rsc.tsx";
+export { default as Rsc, type RscProps } from "./components/rsc.tsx";
 export {
   default as RscPromise,
   type RscPromiseProps,
-} from "./components/RscPromise.tsx";
+} from "./components/rsc_promise.tsx";
