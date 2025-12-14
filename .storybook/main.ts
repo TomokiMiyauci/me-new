bd95e62b68d2635af5569c88fc912df72fb31750
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2025-12-14 13:26:52 +0900

    chore: override vite config for storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,9 +6,24 @@ export default {
   ],
   addons: [
     "@chromatic-com/storybook",
-    // "@storybook/addon-vitest",
     "@storybook/addon-a11y",
     "@storybook/addon-docs",
   ],
   framework: "@storybook/react-vite",
+  viteFinal: (config) => {
+    const plugins = config.plugins?.filter((plugin) => {
+      // Fileter All array plugin. it is rsc plugins
+      if (Array.isArray(plugin)) {
+        return false;
+      }
+
+      return plugin;
+    });
+
+    // Disabled Environment API and rsc plugins
+    config.environments = {};
+    config.plugins = plugins;
+
+    return config;
+  },
 } satisfies StorybookConfig;
