b4a4b38b8b9a72e590981ad5010822c001496f43
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2025-12-23 11:51:57 +0900

    refactor: use dynamic middleware utils

--- src/framework/entry.server.tsx
+++ src/framework/entry.server.tsx
@@ -1,4 +1,5 @@
 import { Router } from "router";
+import { dynamic } from "router/middleware";
 import { init } from "@sentry/deno";
 import { CSP_ENDPOINT } from "~env";
 import { Csp, NonceContext, NonceProvider } from "router/csp";
@@ -7,19 +8,21 @@ import { App, createAssetMiddleware } from "./utils.ts";
 
 init(sentryConfig);
 
+const csp = dynamic<NonceContext>((_, { nonce }) =>
+  new Csp({
+    "default-src": ["'none'"],
+    "script-src": [`'nonce-${nonce}'`, "'unsafe-eval'", "'self'"],
+    "style-src": ["'self'", `'unsafe-inline'`],
+    "img-src": ["https:", "data:"],
+    "report-uri": CSP_ENDPOINT ? [CSP_ENDPOINT] : [],
+    "connect-src": ["ws:", "https:", "http:"],
+  })
+);
+
 const router = new Router<NonceContext>();
 router
   .use(new NonceProvider())
-  .use(
-    new Csp((_, { nonce }) => ({
-      "default-src": ["'none'"],
-      "script-src": [`'nonce-${nonce}'`, "'unsafe-eval'", "'self'"],
-      "style-src": ["'self'", `'unsafe-inline'`],
-      "img-src": ["https:", "data:"],
-      "report-uri": CSP_ENDPOINT ? [CSP_ENDPOINT] : [],
-      "connect-src": ["ws:", "https:", "http:"],
-    })),
-  );
+  .use(csp);
 
 if (import.meta.env.PROD) router.use(await createAssetMiddleware());
 
