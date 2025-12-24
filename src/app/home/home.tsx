53b0ca15378d399e1a715a101fc46724a9d90e99
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2025-12-24 16:51:06 +0900

    feat: add list of contents

--- src/app/home/home.tsx
+++ src/app/home/home.tsx
@@ -5,8 +5,19 @@ import Entry from "@/routes/entry.ts";
 import Layout from "../layout.tsx";
 import { languages } from "@/language.ts";
 import greet from "./greet.json" with { type: "json" };
+import client from "~lib/graphql-client";
+import { BlogDocument } from "@/gql/graphql.ts";
+import Image from "@/components/image/image.tsx";
 
-export default function Home(props: AppProps): JSX.Element {
+export default async function Home(props: AppProps): Promise<JSX.Element> {
+  const { lang, i18n } = props;
+
+  const queryResult = await client.query(BlogDocument, { lang });
+  const blog = queryResult.allBlog[0];
+  const title = blog?.title ?? "";
+  const description = blog?.description;
+  const { t } = i18n;
+
   return (
     <Layout
       {...props}
@@ -25,6 +36,34 @@ export default function Home(props: AppProps): JSX.Element {
             </span>
           </span>
         </div>
+
+        <section>
+          <h2 className="text-center text-3xl mb-8">
+            {t("category.contents")}
+          </h2>
+
+          <ul className="grid md:grid-cols-3">
+            {blog && (
+              <li className="md:col-2 justify-self-center">
+                <a href={resolver.resolve(Entry.Posts, { lang }) ?? undefined}>
+                  <div className="card bg-base-100 shadow-sm max-w-96">
+                    {blog?.coverImage && (
+                      <figure>
+                        <Image fragment={blog.coverImage} />
+                      </figure>
+                    )}
+                    <div className="card-body">
+                      <h2 className="card-title">{title}</h2>
+                      <p>
+                        {description}
+                      </p>
+                    </div>
+                  </div>
+                </a>
+              </li>
+            )}
+          </ul>
+        </section>
       </main>
     </Layout>
   );
