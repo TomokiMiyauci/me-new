f5087a2b83a6ddc204054399b5e599672c947d38
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2025-12-29 11:51:01 +0900

    refactor(react-ogp): split components

---
+++ src/packages/react_ogp/src/object_type.tsx
@@ -0,0 +1,17 @@
+import type { JSX } from "react";
+import OgArticle, { type OgArticleProps } from "./og_article.tsx";
+
+export interface Article {
+  type: "article";
+  article: OgArticleProps;
+}
+
+export type ObjectTypeProps = Article;
+
+export default function ObjectType(props: ObjectTypeProps): JSX.Element {
+  switch (props.type) {
+    case "article": {
+      return <OgArticle {...props.article} />;
+    }
+  }
+}
--- src/packages/react_ogp/src/ogp.tsx
+++ src/packages/react_ogp/src/ogp.tsx
@@ -1,72 +1,29 @@
 import type { JSX } from "react";
-
-interface OgArticle {
-  publishedTime?: string;
-  modifiedTime?: string;
-  section?: string;
-  tags?: string[];
-}
+import OgImage, { type OgImageProps } from "./og_image.tsx";
+import ObjectType, { type Article } from "./object_type.tsx";
 
 interface OgpProps extends BasicMetadata, Article {
 }
 
-interface Article {
-  type: "article";
-  article: OgArticle;
-}
-
 interface BasicMetadata {
   title?: string;
-  image?: string;
   url?: string;
   description?: string;
+  image?: string | OgImageProps;
 }
 
 export default function Ogp(props: OgpProps): JSX.Element {
-  const { title, description, type } = props;
+  const { title, description, type, image } = props;
 
   return (
     <>
       {title && <meta property="og:title" content={title} />}
       {description && <meta property="og:description" content={description} />}
       <meta property="og:type" content={type} />
-      <ObjectTypes {...props} />
+      {image && (typeof image === "string"
+        ? <meta property="og:image" content={image} />
+        : <OgImage {...image} />)}
+      <ObjectType {...props} />
     </>
   );
-}
-
-function OgArticle(props: OgArticle): JSX.Element {
-  const { publishedTime, modifiedTime, section, tags } = props;
-
-  return (
-    <>
-      {publishedTime && (
-        <meta
-          property="article:published_time"
-          content={publishedTime}
-        />
-      )}
-      {modifiedTime && (
-        <meta
-          property="article:modified_time"
-          content={modifiedTime}
-        />
-      )}
-      {section && <meta property="article:section" content={section} />}
-      {tags &&
-        tags.map((tag) => (
-          <meta property="article:tag" content={tag} key={tag} />
-        ))}
-    </>
-  );
-}
-
-type ObjectTypesProps = Article;
-
-function ObjectTypes(props: ObjectTypesProps): JSX.Element {
-  switch (props.type) {
-    case "article": {
-      return <OgArticle {...props.article} />;
-    }
-  }
 }
