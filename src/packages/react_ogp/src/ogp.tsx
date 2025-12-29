7ec16ecdb677359a5a9f9c3597030a1dba12f5b6
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2025-12-29 13:44:36 +0900

    feat(react-ogp): add locale and site name field

--- src/packages/react_ogp/src/ogp.tsx
+++ src/packages/react_ogp/src/ogp.tsx
@@ -6,19 +6,74 @@ interface OgpProps extends BasicMetadata, Article {
 }
 
 interface BasicMetadata {
+  /**
+   * The title of your object as it should appear within the graph, e.g., "The Rock".
+   */
   title?: string;
+
+  /**
+   * The canonical URL of your object that will be used as its permanent ID in the graph, e.g., "https://www.imdb.com/title/tt0117500/".
+   */
   url?: string;
+
+  /**
+   * A one to two sentence description of your object.
+   */
   description?: string;
+
+  /**
+   * An image URL or object which should represent your object within the graph.
+   */
   image?: string | OgImageProps;
+
+  /**
+   * The locale these tags are marked up in. Of the format `language_TERRITORY`.
+   */
+  locale?: string;
+
+  /**
+   * Locales this page is available in.
+   */
+  localeAlternate?: string[] | string;
+
+  /**
+   * If your object is part of a larger web site, the name which should be displayed for the overall site. e.g., "IMDb".
+   */
+  siteName?: string;
+
+  /**
+   * The word that appears before this object's title in a sentence.
+   */
+  determiner?: "a" | "an" | "the" | "auto" | "";
 }
 
 export default function Ogp(props: OgpProps): JSX.Element {
-  const { title, description, type, image } = props;
+  const {
+    title,
+    description,
+    type,
+    image,
+    locale,
+    localeAlternate,
+    siteName,
+    determiner,
+  } = props;
+  const localeAlternates = Array.isArray(localeAlternate)
+    ? localeAlternate
+    : localeAlternate
+    ? [localeAlternate]
+    : [];
 
   return (
     <>
       {title && <meta property="og:title" content={title} />}
       {description && <meta property="og:description" content={description} />}
+      {locale && <meta property="og:locale" content={locale} />}
+      {localeAlternates.map((locale) => (
+        <meta key={locale} property="og:locale:alternate" content={locale} />
+      ))}
+      {siteName && <meta property="og:site_name" content={siteName} />}
+      {determiner && <meta property="og:determiner" content={determiner} />}
       <meta property="og:type" content={type} />
       {image && (typeof image === "string"
         ? <meta property="og:image" content={image} />
