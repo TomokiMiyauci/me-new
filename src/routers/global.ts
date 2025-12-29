8d950c096694161f7319b91436da5611a560ecfe
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2025-12-29 13:28:34 +0900

    fix: instantiate the router at the entry point

--- src/routers/global.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "router";
-import { TrailingSlash } from "router/trailing-slash";
-import Redirect from "@/handlers/redirect/middleware.ts";
-
-export default new Router()
-  .use(new TrailingSlash("never"))
-  .use(new Redirect());
