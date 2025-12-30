5e63895d7e094285e52dc7623633f95866c0d319
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2025-12-30 12:09:09 +0900

    feat(react-meta): add alternates field

--- src/packages/react_meta/src/mod.ts
+++ src/packages/react_meta/src/mod.ts
@@ -1,1 +1,5 @@
-export { default as SeoMeta, type SeoMetaProps } from "./seo_meta.tsx";
+export {
+  type Alternate,
+  default as SeoMeta,
+  type SeoMetaProps,
+} from "./seo_meta.tsx";
--- src/packages/react_meta/src/seo_meta.tsx
+++ src/packages/react_meta/src/seo_meta.tsx
@@ -4,15 +4,29 @@ export interface SeoMetaProps {
   title?: string;
   description?: string;
   canonical?: string;
+  alternates?: Alternate[];
+}
+
+export interface Alternate {
+  hrefLang: string;
+  href: string;
 }
 
 export default function SeoMeta(props: SeoMetaProps): JSX.Element {
-  const { title, description, canonical } = props;
+  const { title, description, canonical, alternates } = props;
   return (
     <>
       {title && <title>{title}</title>}
       {description && <meta name="description" content={description}></meta>}
       {canonical && <link rel="canonical" href={canonical} />}
+      {alternates?.map((alternate) => (
+        <link
+          key={alternate.hrefLang}
+          rel="alternate"
+          hrefLang={alternate.hrefLang}
+          href={alternate.href}
+        />
+      ))}
     </>
   );
 }
